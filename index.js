7762865c06fb37be28aaf75b703636c523bc7825
Jared Hanson <jaredhanson@gmail.com>
2016-09-15 14:52:23 -0700

    Simplify spec manifest.

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-exports = module.exports = function crypto_federation(id) {
-  var map = {
-    'keyring': './xom/keyring',
-    'secret/find': './xom/secret/find'
-  };
-  
-  var mid = map[id];
-  if (mid) {
-    return require(mid);
-  }
-};
-
-exports.used = function(container) {
-  container.add('keyring');
-  container.add('secret/find');
+exports = module.exports = {
+  'keyring': require('./xom/keyring'),
+  'secret/find': require('./xom/secret/find')
 };
